a3cf7e5177914072df61f2ec8ebcda0999e4bfa5
AntonLan <lanqel@gmail.com>
2024-02-20 12:02:31 +0300

    initial projiect

---
+++ Threads-Clone/Threads-Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threads-Clone
+//
+//  Created by Anton Gerasimov on 20.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threads-Clone/Threads-Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads-Clone
+//
+//  Created by Anton Gerasimov on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
