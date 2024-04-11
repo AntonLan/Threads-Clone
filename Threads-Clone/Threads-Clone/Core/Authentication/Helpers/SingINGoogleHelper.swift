03a40d2d4fa8386094dded2dbd2f9da82c74823b
AntonLan <lanqel@gmail.com>
2024-04-11 12:23:04 +0200

    add google sing in

---
+++ Threads-Clone/Threads-Clone/Core/Authentication/Helpers/SingINGoogleHelper.swift
@@ -0,0 +1,16 @@
+//
+//  SingINGoogleHelper.swift
+//  Threads-Clone
+//
+//  Created by Anton Gerasimov on 11.04.2024.
+//
+
+struct GoogleSingInResultModel {
+    var idTocken: String
+    var accessToken: String
+}
+
+final class SingINGoogleHelper {
+    
+
+}
